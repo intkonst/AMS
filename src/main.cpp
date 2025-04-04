28480616ffcd0f5131835d2a025d33a6eec2055e
intkonst <intkonst@gmail.com>
2025-04-04 13:18:39 +0300

    src: mount project file structure

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <thread>
+
+int main (int, char**) {
+    //read config
+    //run thread API
+    //run thread Handler
+    //wait end of threads life 
+    return 0;
+}
