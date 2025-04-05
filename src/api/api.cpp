5a38162b5ed71b818265cdde25d6492af9fa0db3
intkonst <intkonst@gmail.com>
2025-04-05 08:29:56 +0300

    minor edits

--- src/api/api.cpp
+++ src/api/api.cpp
@@ -5,13 +5,13 @@
 
 namespace api {
 void example() {
+    std::cout << "[api]: wait 1 sec." << std::endl;
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     std::cout << "[api]: this text write by api example function" << std::endl;
     return;
 }
 
 void api_main() {
-    //code realization is here
     std::cout << "[api]: api thread id: "<< std::this_thread::get_id() << std::endl;
     example();
     return;
