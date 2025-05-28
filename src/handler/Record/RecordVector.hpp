a5230ccbd0aa4e888b5adf1471c9958fa321332d
kostikgrach <kostikgrach@yandex.ru>
2025-05-28 19:51:13 +0300

    Record classes (beta)

---
+++ src/handler/Record/RecordVector.hpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <nlohmann/json.hpp>
+#include <string>
+
+#include "Record.hpp"
+
+
+struct RecordVector : std::vector<Record> {
+    RecordVector(nlohmann::json& source);
+    RecordVector(const RecordVector& other);
+
+    RecordVector& operator=(const RecordVector& other);
+
+    std::vector<std::string> insertQuery() const;
+
+    ~RecordVector() = default;
+};
